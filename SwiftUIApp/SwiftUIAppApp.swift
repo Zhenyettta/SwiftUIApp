eb8a755cdbcc1ee3c973e9e9b34a3907fb348c78
Yevhen Anisimov <yevhen.anisimov@ukma.edu.ua>
2025-04-20 15:58:23 +0300

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Євген Анісімов on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
